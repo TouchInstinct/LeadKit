f593566ed6be56e189457c85860089ab0a7fafa8
Николай Ашанин <nv.ashanin@gmail.com>
2016-01-24 01:44:30 +0300

    add logger to base library

--- iOS-Base-Library/Pods/Target Support Files/Pods-iOS-Base-Library/Pods-iOS-Base-Library-frameworks.sh
+++ iOS-Base-Library/Pods/Target Support Files/Pods-iOS-Base-Library/Pods-iOS-Base-Library-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-iOS-Base-Library/Alamofire.framework"
+  install_framework "Pods-iOS-Base-Library/CocoaLumberjack.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-iOS-Base-Library/Alamofire.framework"
+  install_framework "Pods-iOS-Base-Library/CocoaLumberjack.framework"
 fi
--- iOS-Base-Library/iOS-Base-Library/AppDelegate.swift
+++ iOS-Base-Library/iOS-Base-Library/AppDelegate.swift
@@ -7,16 +7,18 @@
 //
 
 import UIKit
+import CocoaLumberjack
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        DDLogVerbose("Verbose")
+
         return true
     }
 
