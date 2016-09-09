6cea000af94f5264a601f4252e8710163c0b45bf
Ivan Smolin <ivan.smolin@touchin.ru>
2016-09-09 10:13:46 +0300

    refactor string size calculation

--- LeadKit/LeadKit/Extensions/NSString/NSString+SizeCalculation.swift
+++ LeadKit/LeadKit/Extensions/NSString/NSString+SizeCalculation.swift
@@ -8,77 +8,57 @@
 
 import UIKit
 
-extension NSString {
+/**
+ *  Struct for holding result of string size calculation
+ */
+public struct StringSizeCalculationResult {
 
-    /**
-     method which calculates string height based on given width and character attributes
+    public let size: CGSize
+    public let fontLineHeight: CGFloat?
 
-     - parameter width:      maximum width of string
-     - parameter attributes: dictionary with string character attributes
+}
 
-     - returns: string height
-     */
-    public func heightWith(width: CGFloat, attributes: [String: AnyObject]?) -> CGFloat {
-        return self.boundingRectWithSize(CGSize(width: width, height: CGFloat.max),
-                                         options: [.UsesLineFragmentOrigin, .UsesFontLeading],
-                                         attributes: attributes,
-                                         context: nil).size.height
-    }
+public extension StringSizeCalculationResult {
 
-    /**
-     method which calculates required number of lines to fit string with given width and character attributes
+    public var numberOfLines: UInt? {
+        if let lineHeight = fontLineHeight {
+            let lineHeightRounded = Double(lineHeight).roundValue(withPersicion: 2)
 
-     - parameter width:      maximum width of string
-     - parameter attributes: dictionary with string character attributes
+            let heightRounded = Double(size.height).roundValue(withPersicion: 2)
 
-     - returns: minimum number of lines
-     */
-    public func numberOfLinesWith(width: CGFloat, attributes: [String: AnyObject]) -> UInt {
-        guard let font = attributes[NSFontAttributeName] as? UIFont else {
-            preconditionFailure("Value for NSFontAttributeName should be defined in attributes")
+            let numberOfLines = ceil(heightRounded / lineHeightRounded)
+
+            return UInt(numberOfLines)
         }
 
-        let lineHeight = font.lineHeight
-
-        let lineHeightRounded = Double(lineHeight).roundValue(withPersicion: 2)
-
-        let height = heightWith(width, attributes: attributes)
-
-        let heightRounded = Double(height).roundValue(withPersicion: 2)
-
-        let numberOfLines = ceil(heightRounded / lineHeightRounded)
-
-        return UInt(numberOfLines)
+        return nil
     }
 
-    /**
-     method which calculates string width based on given character attributes
+}
 
-     - parameter attriutes: dictionary with string character attributes
-
-     - returns: string width
-     */
-    public func widthWith(attriutes: [String: AnyObject]) -> CGFloat {
-        return CGFloat(ceil(Double(sizeWithAttributes(attriutes).width)))
-    }
+public extension NSString {
 
     /**
-     method which calculates text size based on given character attributes
+     method which calculates string size based on given character attributes and (optional) max width and height
 
-     - parameter width:      maximum width of text
-     - parameter height:     maximum height of text
      - parameter attributes: dictionary with string character attributes
+     - parameter maxWidth:   maximum width of text
+     - parameter maxHeight:  maximum height of text
 
-     - returns: text size
+     - returns: string size calculation result
      */
-    public func sizeWith(maxWidth width: CGFloat = CGFloat.max,
-                                  maxHeight height: CGFloat = CGFloat.max,
-                                            attributes: [String: AnyObject]?) -> CGSize {
+    public func sizeWith(attributes attributes: [String: AnyObject]?,
+                                    maxWidth: CGFloat = CGFloat.max,
+                                    maxHeight: CGFloat = CGFloat.max) -> StringSizeCalculationResult {
 
-        return boundingRectWithSize(CGSize(width: width, height: height),
-                                    options: .UsesLineFragmentOrigin,
-                                    attributes: attributes,
-                                    context: nil).size
+        let size = boundingRectWithSize(CGSize(width: maxWidth, height: maxHeight),
+                                        options: [.UsesLineFragmentOrigin, .UsesFontLeading],
+                                        attributes: attributes,
+                                        context: nil).size
+
+        let fontLineHeight = (attributes?[NSFontAttributeName] as? UIFont)?.lineHeight
+        
+        return StringSizeCalculationResult(size: size, fontLineHeight: fontLineHeight)
     }
     
 }
