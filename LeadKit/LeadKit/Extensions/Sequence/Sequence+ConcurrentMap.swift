c8114d92ec1497eda170c49fbf368fc7bccac125
Ivan Smolin <ivan.smolin@touchin.ru>
2016-12-29 16:12:23 +0300

    fully use rx for concurrent array processing

--- LeadKit/LeadKit/Extensions/Sequence/Sequence+ConcurrentMap.swift
+++ LeadKit/LeadKit/Extensions/Sequence/Sequence+ConcurrentMap.swift
@@ -8,81 +8,44 @@
 
 import RxSwift
 
-public typealias ConcurrentMapCancelClosure = () -> ()
-
 public extension Sequence {
 
     /// Method which asynchronous transforms sequence using given transform closure
     /// and given number of concurrent operations
     ///
     /// - Parameters:
-    ///   - transform: Transform closure
     ///   - concurrentOperationCount: Number of concurrent operations
-    ///   - completion: Completion handler with results of transform
-    /// - Returns: Closure whitch can be called to cancel asynchronous operation
-    @discardableResult
-    func concurrentMap<R>(transform: @escaping ((Iterator.Element) throws -> R),
-                       concurrentOperationCount: Int = ProcessInfo.processInfo.activeProcessorCount,
-                       completion: @escaping (([R]) -> ())) -> ConcurrentMapCancelClosure {
+    ///   - qos: Target global dispatch queue, by quality of service class.
+    ///   - transform: Transform closure
+    /// - Returns: Observable of array which contains transform return type
+    func concurrentRxMap<R>(concurrentOperationCount: Int = ProcessInfo.processInfo.activeProcessorCount,
+                         qos: DispatchQoS = .default,
+                         transform: @escaping ((Iterator.Element) throws -> R)) -> Observable<[R]> {
 
         let operationsCount = Swift.max(1, concurrentOperationCount)
-
-        let operationQueue = OperationQueue()
-        operationQueue.maxConcurrentOperationCount = operationsCount
 
         let array = Array(self)
 
         let step = Int(ceil(Double(array.count) / Double(operationsCount)))
         let numberOfSlices = Int(ceil(Double(array.count) / Double(step)))
 
-        DispatchQueue.global().async {
-            let operations: [ResultOperation<[R]>] = (0..<numberOfSlices).map {
-                let start = $0 * step
-                let end = Swift.min(start + step, array.count)
+        let indexedRanges: [(idx: Int, range: CountableRange<Int>)] = (0..<numberOfSlices).map {
+            let start = $0 * step
+            let end = Swift.min(start + step, array.count)
 
-                return ResultOperation { try array[start..<end].map(transform) }
-            }
-
-            operationQueue.addOperations(operations, waitUntilFinished: true)
-
-            var results: [R] = [] // var is used for performance optimization
-
-            let operationsResults = operations.flatMap { $0.result }
-
-            for operationResult in operationsResults {
-                results += operationResult
-            }
-
-            completion(results)
+            return ($0, start..<end)
         }
 
-        return {
-            operationQueue.cancelAllOperations()
-        }
-    }
+        let scheduler = ConcurrentDispatchQueueScheduler(qos: qos)
 
-}
-
-public extension Sequence {
-
-    /// Reactive version of concurrentMap<R>(transform:concurrentOperationCount:completion:)
-    ///
-    /// - Parameters:
-    ///   - concurrentOperationCount: Number of concurrent operations
-    ///   - transform: Transform closure
-    /// - Returns: Observable of transform return type
-    func concurrentRxMap<R>(concurrentOperationCount: Int = ProcessInfo.processInfo.activeProcessorCount,
-                         transform: @escaping ((Iterator.Element) throws -> R)) -> Observable<[R]> {
-
-        return Observable<[R]>.create { observer in
-            let disposeHandler = self.concurrentMap(transform: transform,
-                                                    concurrentOperationCount: concurrentOperationCount) {
-                                                        observer.onNext($0)
-                                                        observer.onCompleted()
+        return Observable.from(indexedRanges)
+            .flatMap { indexedRange -> Observable<(idx: Int, results: [R])> in
+                return Observable.just(indexedRange)
+                    .observeOn(scheduler)
+                    .map { (idx: $0.idx, results: try array[$0.range].map(transform)) }
             }
-            
-            return Disposables.create(with: disposeHandler)
-        }
+            .toArray()
+            .map { $0.sorted { $0.0.idx < $0.1.idx }.flatMap { $0.results } }
     }
     
 }
