48ecdcc33610c082ba70cca37b064ff8dc21e6b1
Ivan Smolin <ivan.smolin@touchin.ru>
2017-04-05 15:55:50 +0300

    initial placeholders support in pagination wrapper. binding to view model in pagination wrapper

--- LeadKit/LeadKit/Classes/Pagination/PaginationTableViewWrapper.swift
+++ LeadKit/LeadKit/Classes/Pagination/PaginationTableViewWrapper.swift
@@ -21,6 +21,7 @@
 //
 
 import UIKit
+import RxSwift
 
 public protocol PaginationTableViewWrapperDelegate: class {
 
@@ -33,21 +34,123 @@ public protocol PaginationTableViewWrapperDelegate: class {
     func paginationWrapper(wrapper: PaginationTableViewWrapper<Cursor, Self>,
                            didReload allItems: [Cursor.Element])
 
+    func emptyPlaceholder(forPaginationWrapper wrapper: PaginationTableViewWrapper<Cursor, Self>) -> UIView
+
+    func errorPlaceholder(forPaginationWrapper wrapper: PaginationTableViewWrapper<Cursor, Self>,
+                          forError error: Error) -> UIView
+
+}
+
+public extension PaginationTableViewWrapperDelegate {
+
+    func emptyPlaceholder(forPaginationWrapper wrapper: PaginationTableViewWrapper<Cursor, Self>) -> UIView {
+        let placeholder = UIView()
+
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "There is nothing here"
+
+        placeholder.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: placeholder.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: placeholder.centerYAnchor).isActive = true
+
+        return placeholder
+    }
+
+    func errorPlaceholder(forPaginationWrapper wrapper: PaginationTableViewWrapper<Cursor, Self>,
+                          forError error: Error) -> UIView {
+
+        let placeholder = UIView()
+
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "An error has occurred"
+
+        placeholder.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: placeholder.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: placeholder.centerYAnchor).isActive = true
+
+        return placeholder
+    }
+
 }
 
 public class PaginationTableViewWrapper<C: ResettableCursorType, D: PaginationTableViewWrapperDelegate>
 where D.Cursor == C {
 
+    public typealias PlaceholderTransform = (UIView, CGPoint) -> Void
+
     private let tableView: UITableView
-
+    private let placeholdersContainerView: UIView
     private let paginationViewModel: PaginationViewModel<C>
-
     private weak var delegate: D?
 
-    public init(tableView: UITableView, cursor: C, delegate: D) {
+    public var placeholderTransformOnScroll: PlaceholderTransform = { view, offset in
+        var newFrame = view.frame
+        newFrame.origin.y = -offset.y
+
+        view.frame = newFrame
+    }
+
+    private let disposeBag = DisposeBag()
+
+    public init(tableView: UITableView, placeholdersContainer: UIView, cursor: C, delegate: D) {
         self.tableView = tableView
+        self.placeholdersContainerView = placeholdersContainer
         self.paginationViewModel = PaginationViewModel(cursor: cursor)
         self.delegate = delegate
+
+        paginationViewModel.state.drive(onNext: { [weak self] state in
+            switch state {
+            case .initial:
+                self?.onInitialState()
+            case .loading:
+                self?.onLoadingState()
+            case .loadingMore:
+                self?.onLoadingMoreState()
+            case .results(let newItems, let after):
+                self?.onResultsState(newItems: newItems, afterState: after)
+            case .error(let error, let after):
+                self?.onErrorState(error: error, afterState: after)
+            case .empty:
+                self?.onEmptyState()
+            case .exhausted:
+                self?.onExhaustedState()
+            }
+        })
+        .addDisposableTo(disposeBag)
+    }
+
+    public func reload() {
+        paginationViewModel.load(.reload)
+    }
+
+    private func onInitialState() {
+        //
+    }
+
+    private func onLoadingState() {
+        //
+    }
+
+    private func onLoadingMoreState() {
+        //
+    }
+
+    private func onResultsState(newItems: [C.Element], afterState: PaginationViewModel<C>.State) {
+        //
+    }
+
+    private func onErrorState(error: Error, afterState: PaginationViewModel<C>.State) {
+        //
+    }
+
+    private func onEmptyState() {
+        //
+    }
+
+    private func onExhaustedState() {
+        //
     }
 
 }
