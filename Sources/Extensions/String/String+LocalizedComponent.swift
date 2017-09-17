397829f07b599d24ea3c4198bc811b1171c375fd
Igor <Kislyuk>
2017-09-17 17:43:55 +0300

    Add LocalizedComponent + tests

---
+++ Sources/Extensions/String/String+LocalizedComponent.swift
@@ -0,0 +1,52 @@
+//
+//  Copyright (c) 2017 Touch Instinct
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+
+public extension String {
+
+    /// Pass value and proper naming string in russian
+    /// - returns: string of proper count naming
+    static func localizedComponent(value: Int,
+                                   stringOne: String,
+                                   stringTwo: String,
+                                   stringMany: String) -> String {
+
+        if (11...14).contains(value) {
+            return stringMany
+        } else {
+            let lastDigit = value % 10
+
+            switch lastDigit {
+            case 1:
+                return stringOne
+            case 2...4:
+                return stringTwo
+            case 5...9, 0:
+                return stringMany
+            default:
+                return ""
+            }
+        }
+    }
+
+}
