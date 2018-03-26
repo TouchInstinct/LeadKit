751e0a51d8efd2aecf0b3d85e29a534238d13ea0
Ivan Smolin <ivan.smolin@touchin.ru>
2018-03-26 09:23:04 +0300

    number formatting service

---
+++ Sources/Extensions/NumberFormattingService/NumberFormattingService+DefaultImplementation.swift
@@ -0,0 +1,64 @@
+//
+//  Copyright (c) 2018 Touch Instinct
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the Software), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+
+public extension NumberFormattingService {
+
+    /// Computed static property. Use only once for `formatters` field implementation!
+    static var computedFormatters: [NumberFormatType: NumberFormatter] {
+        return Dictionary(uniqueKeysWithValues: NumberFormatType.allValues.map { ($0, $0.numberFormatter) })
+    }
+
+    static func numberFormatter(for format: NumberFormatType) -> NumberFormatter {
+        guard let formatter = formatters[format] else {
+            fatalError("Unregistered number formatter for \(format)")
+        }
+
+        return formatter
+    }
+
+    static func string(from number: NSNumber, format: NumberFormatType, defaultString: String = "") -> String {
+        return numberFormatter(for: format).string(from: number) ?? defaultString
+    }
+
+    static func string(from number: Decimal, format: NumberFormatType, defaultString: String = "") -> String {
+        return string(from: NSDecimalNumber(decimal: number), format: format, defaultString: defaultString)
+    }
+
+    static func string(from number: Int, format: NumberFormatType, defaultString: String = "") -> String {
+        return string(from: NSNumber(value: number), format: format, defaultString: defaultString)
+    }
+
+    static func string(from number: Int64, format: NumberFormatType, defaultString: String = "") -> String {
+        return string(from: NSNumber(value: number), format: format, defaultString: defaultString)
+    }
+
+    static func string(from number: Float, format: NumberFormatType, defaultString: String = "") -> String {
+        return string(from: NSNumber(value: Double(number)), format: format, defaultString: defaultString)
+    }
+
+    static func string(from number: Double, format: NumberFormatType, defaultString: String = "") -> String {
+        return string(from: NSNumber(value: number), format: format, defaultString: defaultString)
+    }
+
+}
---
+++ Sources/Protocols/NumberFormattingService/NumberFormattingService.swift
@@ -0,0 +1,92 @@
+//
+//  Copyright (c) 2018 Touch Instinct
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the Software), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+
+/// Protocol that contains number formatting functions for often used number types.
+public protocol NumberFormattingService {
+
+    associatedtype NumberFormatType: NumberFormat
+
+    static var formatters: [NumberFormatType: NumberFormatter] { get }
+
+    /// Method returns NumberFormatter for given NumberFormatType.
+    ///
+    /// - Parameter format: A number format under which should be configured NumberFormatter.
+    /// - Returns: Configured NumberFormatter instance.
+    static func numberFormatter(for format: NumberFormatType) -> NumberFormatter
+
+    /// Method format number in given format. If formatting fails - it returns default string.
+    ///
+    /// - Parameters:
+    ///   - number: A number to format.
+    ///   - format: A format that should be used for number formatting.
+    ///   - defaultString: Default string if formatting will fail.
+    /// - Returns: An string that contains formatted number or default string if formatting did fail.
+    static func string(from number: NSNumber, format: NumberFormatType, defaultString: String) -> String
+
+    /// Method format number in given format. If formatting fails - it returns default string.
+    ///
+    /// - Parameters:
+    ///   - number: A number to format.
+    ///   - format: A format that should be used for number formatting.
+    ///   - defaultString: Default string if formatting will fail.
+    /// - Returns: An string that contains formatted number or default string if formatting did fail.
+    static func string(from number: Int, format: NumberFormatType, defaultString: String) -> String
+
+    /// Method format number in given format. If formatting fails - it returns default string.
+    ///
+    /// - Parameters:
+    ///   - number: A number to format.
+    ///   - format: A format that should be used for number formatting.
+    ///   - defaultString: Default string if formatting will fail.
+    /// - Returns: An string that contains formatted number or default string if formatting did fail.
+    static func string(from number: Int64, format: NumberFormatType, defaultString: String) -> String
+
+    /// Method format number in given format. If formatting fails - it returns default string.
+    ///
+    /// - Parameters:
+    ///   - number: A number to format.
+    ///   - format: A format that should be used for number formatting.
+    ///   - defaultString: Default string if formatting will fail.
+    /// - Returns: An string that contains formatted number or default string if formatting did fail.
+    static func string(from number: Float, format: NumberFormatType, defaultString: String) -> String
+
+    /// Method format number in given format. If formatting fails - it returns default string.
+    ///
+    /// - Parameters:
+    ///   - number: A number to format.
+    ///   - format: A format that should be used for number formatting.
+    ///   - defaultString: Default string if formatting will fail.
+    /// - Returns: An string that contains formatted number or default string if formatting did fail.
+    static func string(from number: Double, format: NumberFormatType, defaultString: String) -> String
+
+    /// Method format number in given format. If formatting fails - it returns default string.
+    ///
+    /// - Parameters:
+    ///   - number: A number to format.
+    ///   - format: A format that should be used for number formatting.
+    ///   - defaultString: Default string if formatting will fail.
+    /// - Returns: An string that contains formatted number or default string if formatting did fail.
+    static func string(from number: Decimal, format: NumberFormatType, defaultString: String) -> String
+
+}
