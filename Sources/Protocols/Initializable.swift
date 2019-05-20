b2016fa157fb1efee1fbe67d94b972edc3499e4a
Aliona <alionka1225@yandex.ru>
2019-05-20 20:18:01 +0300

    Fix Artur's comments

--- Sources/Protocols/Initializable.swift
+++ Sources/Protocols/Initializable.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2017 Touch Instinct
+//  Copyright (c) 2019 Touch Instinct
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@ protocol Initializable {
 
 extension Initializable {
 
-    static func instanceArray(count: Int) -> [Self] {
+    static func instantiateArray(count: Int) -> [Self] {
         var array: [Self] = []
         (0..<count).forEach { _ in
             array.append(Self())
