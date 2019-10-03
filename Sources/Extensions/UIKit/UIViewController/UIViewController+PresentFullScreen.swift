090c066a75a34bd691980dd15a8275987edaa9ed
Sergey Kopytov <kopytov@me.com>
2019-10-03 23:15:18 +0300

    add comment

--- Sources/Extensions/UIKit/UIViewController/UIViewController+PresentFullScreen.swift
+++ Sources/Extensions/UIKit/UIViewController/UIViewController+PresentFullScreen.swift
@@ -1,6 +1,12 @@
 import UIkit
 
 public extension UIViewController {
+    /// Method present new ViewController modally in full screen mode
+    ///
+    /// - Parameters:
+    ///   - viewController: ViewController to present
+    ///   - animated: ppresent with animation
+    ///   - completion: completion block after present ended
     func presentFullScreen(_ viewController: UIViewController,
                            animated: Bool = true,
                            completion: (() -> Void)? = nil) {
