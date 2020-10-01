70114359050975e12e540d376ae231f06ca3298d
Boyko Mihail <mikhail.boyko@touchin.ru>
2020-10-01 19:46:40 +0300

    cherry-pick ccc085df3c5df7bd16f99188055b3036449d4c75

---
+++ Sources/Classes/Controllers/BaseOrientationController.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+open class BaseOrientationController: UIViewController {
+
+    /// Ability to set forced screen orientation
+    open var forcedInterfaceOrientation: UIInterfaceOrientation?
+
+    open override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+            switch forcedInterfaceOrientation {
+            case .landscapeLeft:
+                return .landscapeLeft
+
+            case .landscapeRight:
+                return .landscapeRight
+
+            case .portrait:
+                return .portrait
+
+            case .portraitUpsideDown:
+                return .portraitUpsideDown
+
+            default:
+                return super.supportedInterfaceOrientations
+            }
+        }
+
+        open override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+            return forcedInterfaceOrientation ?? super.preferredInterfaceOrientationForPresentation
+        }
+}
