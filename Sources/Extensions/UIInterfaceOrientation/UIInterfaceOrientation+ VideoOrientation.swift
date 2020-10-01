9ae23ecef4521a33949abfb3568066cf93cdba68
Boyko Mihail <mikhail.boyko@touchin.ru>
2020-10-01 20:28:46 +0300

    Add videoOrientation extension

---
+++ Sources/Extensions/UIInterfaceOrientation/UIInterfaceOrientation+ VideoOrientation.swift
@@ -0,0 +1,9 @@
+//
+//  UIInterfaceOrientation+ VideoOrientation.swift
+//  LeadKit iOS
+//
+//  Created by Mikhail Boyko on 01.10.2020.
+//  Copyright © 2020 Touch Instinct. All rights reserved.
+//
+
+import Foundation
