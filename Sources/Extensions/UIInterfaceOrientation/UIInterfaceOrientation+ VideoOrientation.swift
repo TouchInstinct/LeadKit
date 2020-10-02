0d51ccb9084661290da7379d45ed2c8d5ec871c9
Boyko Mihail <mikhail.boyko@touchin.ru>
2020-10-02 15:53:47 +0300

    Fix PR issue

--- Sources/Extensions/UIInterfaceOrientation/UIInterfaceOrientation+ VideoOrientation.swift
+++ Sources/Extensions/UIInterfaceOrientation/UIInterfaceOrientation+ VideoOrientation.swift
@@ -4,17 +4,21 @@ import AVFoundation
 public extension UIInterfaceOrientation {
     
     var videoOrientation: AVCaptureVideoOrientation {
-        switch self {
-        case .portrait:
-            return .portrait
-        case .landscapeLeft:
-            return .landscapeLeft
-        case .landscapeRight:
-            return .landscapeRight
-        case .portraitUpsideDown:
-            return .portraitUpsideDown
-        case .unknown:
-            return .portrait
+            switch self {
+            case .portrait, .unknown:
+                return .portrait
+
+            case .landscapeLeft:
+                return .landscapeLeft
+
+            case .landscapeRight:
+                return .landscapeRight
+
+            case .portraitUpsideDown:
+                return .portraitUpsideDown
+                
+            @unknown default:
+                return .portrait
+            }
         }
-    }
 }
