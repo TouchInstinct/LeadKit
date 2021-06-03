75e38174185ae75971e62a5e27fde696d94551a3
Boyko Mihail <mikhail.boyko@touchin.ru>
2021-06-03 16:29:42 +0300

    fix: Large title

--- TIUIElements/Sources/Helpers/HeaderTransitionDelegate.swift
+++ TIUIElements/Sources/Helpers/HeaderTransitionDelegate.swift
@@ -58,6 +58,7 @@ extension HeaderTransitionDelegate: UITableViewDelegate {
         
         if isStartSet {
             startOffset = -(headerHandler.tableView.contentOffset.y)
+            startOffset = startOffset < 0 ? 0 : startOffset
             navigationBarHeight = headerHandler.navigationController?.navigationBar.bounds.height ?? 0
             if #available(iOS 13.0, *) {
                 statusBarHeight = headerHandler.view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
@@ -67,9 +68,10 @@ extension HeaderTransitionDelegate: UITableViewDelegate {
             isStartSet = false
         }
 
+        let prefersLargeTitles = headerHandler.navigationController?.navigationBar.prefersLargeTitles ?? false
         let offsetY = scrollView.contentOffset.y + startOffset
-          
-        let isHidden = offsetY <= (largeHeaderView.frame.height + navigationBarHeight - statusBarHeight)
+        let navigayionBarOffset = prefersLargeTitles ? navigationBarHeight - statusBarHeight : 0
+        let isHidden = offsetY <= (largeHeaderView.frame.height + navigayionBarOffset)
         headerHandler.navigationController?.navigationBar.topItem?.titleView?.isHidden = isHidden
     }
 }
