d6fe349189aaf79b5c93f502b7bdc06d2a3e909b
Nikita Semenov <castlelecs@gmail.com>
2022-10-11 11:07:08 +0300

    fix: spm dependencies

--- Package.swift
+++ Package.swift
@@ -84,7 +84,7 @@ let package = Package(
     .target(name: "TIAuth", dependencies: ["TIFoundationUtils"], path: "TIAuth/Sources"),
     
     //MARK: - Skolkovo
-    .target(name: "TIEcommerce", dependencies: ["TIFoundationUtils", "TISwiftUtils", "TINetworking"], path: "TIEcommerce/Sources"),
+    .target(name: "TIEcommerce", dependencies: ["TIFoundationUtils", "TISwiftUtils", "TINetworking", "TIUIKitCore", "TIUIElements"], path: "TIEcommerce/Sources"),
     
     // MARK: - Tests
     
